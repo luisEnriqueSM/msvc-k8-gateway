9d8b72f5424f20e43862f11e2c865658703699a3
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-06-14 15:32:49 -0600

    First Commit

---
+++ src/main/java/org/k8/springcloud/msvc/gateway/msvc_k8_gateway/MsvcK8GatewayApplication.java
@@ -0,0 +1,13 @@
+package org.k8.springcloud.msvc.gateway.msvc_k8_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcK8GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcK8GatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/k8/springcloud/msvc/gateway/msvc_k8_gateway/MsvcK8GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.k8.springcloud.msvc.gateway.msvc_k8_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcK8GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
