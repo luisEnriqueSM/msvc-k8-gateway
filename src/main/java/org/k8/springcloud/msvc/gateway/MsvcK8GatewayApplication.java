439deb0f36a121847b513ae22c1ce05f717d8227
luis_enriquesm96@hotmail.com <luis_enriquesm96@hotmail.com>
2025-06-14 15:51:21 -0600

    Add spring-cloud-starter-kubernetes-client and spring-cloud-starter-kubernetes-client-loadbalancer dependencies. Add @EnableDiscoveryClient annotation for Kubernetes discovery client.

--- src/main/java/org/k8/springcloud/msvc/gateway/MsvcK8GatewayApplication.java
+++ src/main/java/org/k8/springcloud/msvc/gateway/MsvcK8GatewayApplication.java
@@ -1,8 +1,10 @@
-package org.k8.springcloud.msvc.gateway.msvc_k8_gateway;
+package org.k8.springcloud.msvc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsvcK8GatewayApplication {
 
